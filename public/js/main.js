9f456adca9766f2d7fdcc730f8985b2039e4c8d6
M16 <Adilmohamedmohamed97@gmail.com>
2021-05-14 10:18:57 +0100

    Method

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+// 1
+let Nom = prompt('Quel est ton prénom ?');
+alert('Bonjour '+Nom );
+// 2
+let Age = prompt('Quel age as-tu ?');
+alert('Tu as '+Age);
+// 3
+let nom = prompt('Quel est ton nom ?');
+let Famille = prompt('Quel est ton nom de famille ?');
+let NomFamille = (nom+' '+Famille)
+alert('Bonjour '+NomFamille);
+// 4
+let noe = prompt('Quel est ton nom ?');
+let eFamille = prompt('Quel est ton nom de famille ?');
+alert(`Bonjour ${nom.charAt(0).toUpperCase()}${nom.substr(1)}${Famille}`);
